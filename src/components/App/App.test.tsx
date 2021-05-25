fe08fff33a425385f2ebacac08220fe23a0f97e1
Noble Obioma <nobioma1@gmail.com>
2021-05-25 23:46:10 +0100

    ft: create react typescript app with tailwindcss

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders this is app', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/This is app/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/components/App/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="App">This is app</div>;
+};
+
+export default App;
